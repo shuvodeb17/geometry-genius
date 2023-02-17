b3fb172853f02f7dcf445c4c226c12e17c2b9c13
shuvodeb17 <shuvodeb17014@gmail.com>
2023-02-17 11:14:16 -0800

    triangle calculation complate

--- js/app.js
+++ js/app.js
@@ -2,13 +2,31 @@
 document
   .getElementById("triangle-calculate-btn")
   .addEventListener("click", function () {
+    
+    // get input field value
     const triangleBaseInput = getInputValue("triangle-base-input");
     const triangleHeightInput = getInputValue("triangle-height-input");
 
-    // input text set
-    inputValueSetText("triangle-value-one", triangleBaseInput);
-    inputValueSetText("triangle-value-two", triangleHeightInput);
+    let triangleResult;
+    if (
+      inputFieldValueLength("triangle-base-input") === 0 ||
+      inputFieldValueLength("triangle-height-input") === 0
+    ) {
+      alert("input field empty");
+      return;
+    } else if (isNaN(triangleBaseInput) || isNaN(triangleHeightInput)) {
+      alert("Please enter number");
+    } else if (triangleBaseInput < 0 || triangleHeightInput < 0) {
+      alert("Please enter positive number");
+    } else {
+      // calculate
+      triangleResult = 0.5 * triangleBaseInput * triangleHeightInput;
 
-    const triangleResult = 0.5 * triangleBaseInput * triangleHeightInput;
-    console.log(triangleResult)
+      // input value set on the element
+      setInnerText("triangle-value-one", triangleBaseInput);
+      setInnerText("triangle-value-two", triangleHeightInput);
+    }
+
+    // show result area calculation
+    showResultAreaCalculation("order-list", "div", "Triangle", triangleResult);
   });
--- js/ulilites.js
+++ js/ulilites.js
@@ -1,10 +1,31 @@
+// get input field value
 function getInputValue(inputFieldId) {
   const input = document.getElementById(inputFieldId);
   const convertInput = parseFloat(input.value);
   return convertInput;
 }
 
-function inputValueSetText(elementId, value) {
-    const element = document.getElementById(elementId);
-    element.innerText = value;
+// input field length
+function inputFieldValueLength(inputId) {
+    const input = document.getElementById(inputId).value.length;
+    return input
+}
+
+// setInnerText
+function setInnerText(elementId, value) {
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+}
+
+// show result area calculation
+function showResultAreaCalculation(orderList, createElement, calculateResultName, result) {
+  const ol = document.getElementById(orderList);
+  const createDiv = document.createElement(createElement);
+  createDiv.className = "ol-child";
+  createDiv.innerHTML = `
+        <li>${calculateResultName}</li>
+        <p>${result.toFixed(2)}Cm<sup>2</sup></p>
+        <button class="btn btn-primary">Convert to m<sup>2</sup></button>
+    `;
+  ol.appendChild(createDiv);
 }
